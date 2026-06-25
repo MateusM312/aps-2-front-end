cebeec074b386901e086c6c281cf47d77137373b
MateusM312 <mateusmendes2626@gmail.com>
2026-06-25 16:14:42 -0300

    hello world

---
+++ html_projects_during_class/aula-12/introducao-javascript/js/form-adiciona.js
@@ -0,0 +1,15 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+// botaoAdicionar.addEventListener("click", function(event){
+//     event.preventDefault();
+//     console.log("fui clicado");
+// })
+
+function mostrarM(){
+    event.preventDefault(); 
+    console.log("Olá, o botão foi clicado");
+}
+
+// botaoAdicionar.addEventListener("click", mostrarM());
+
+// botaoAdicionar.addEventListener("click", mostrarM(event));
